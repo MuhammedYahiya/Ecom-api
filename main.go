a37e30f356a4ab237564d4aca7c4277a39b773c4
MuhammedYahiya <yahiyahmhd@gmail.com>
2024-02-27 20:06:53 +0530

    create a  database connection

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/MuhammedYahiya/Ecom-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	utils.ConnectDb()
 
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, Gin!")
